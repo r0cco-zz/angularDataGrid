00e8dfa0b0b95443375667f862102439649c5c3e
r0cco <Rocco.Riccardi.42@gmail.com>
2017-08-13 01:10:03 -0400

    add project to repo

---
+++ angularDataGrid/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace angularDataGrid.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ angularDataGrid/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace angularDataGrid.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
